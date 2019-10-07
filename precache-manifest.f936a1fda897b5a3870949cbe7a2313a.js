13dcc25b83e63d5d11dcd462c1352e9e3a81fa8d
Juan <jjrosalesuci@gmail.com>
2019-10-06 23:30:58 -0300

    Updates

---
+++ precache-manifest.f936a1fda897b5a3870949cbe7a2313a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c961e36916d08bbec5302e467b7654f",
+    "url": "/jjrosalesuci.github.io/dcpropiedades/index.html"
+  },
+  {
+    "revision": "11bdb099e79c1d42ed20",
+    "url": "/jjrosalesuci.github.io/dcpropiedades/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "ddf516109c10ad81b7d5",
+    "url": "/jjrosalesuci.github.io/dcpropiedades/static/js/2.4c68be39.chunk.js"
+  },
+  {
+    "revision": "11bdb099e79c1d42ed20",
+    "url": "/jjrosalesuci.github.io/dcpropiedades/static/js/main.bf754028.chunk.js"
+  },
+  {
+    "revision": "65dfb9281ace7921eb8d",
+    "url": "/jjrosalesuci.github.io/dcpropiedades/static/js/runtime~main.4f4e20c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jjrosalesuci.github.io/dcpropiedades/static/media/logo.5d5d9eef.svg"
+  }
+]);
